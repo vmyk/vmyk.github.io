aaa736086ef3c386e77d3364d4f3e05ae4bb5f69
Miguel Silva <85355352+vmyk@users.noreply.github.com>
2023-07-17 22:06:27 +0100

    Updated background color

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function updateTime() {
-    const clockElement = document.getElementById("clock");
-    const now = new Date();
-    const options = {
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      hour12: false,
-      timeZone: "Europe/Lisbon"
-    };
-    const timeString = now.toLocaleTimeString("en-US", options);
-    clockElement.textContent = timeString;
-  
-    setTimeout(updateTime, 1000); // Update the clock every second
-  }
-  
-  updateTime();
-  
+  const clockElement = document.getElementById("clock");
+  const now = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+    timeZone: "Europe/Lisbon",
+  };
+  const timeString = now.toLocaleTimeString("en-US", options);
+  clockElement.textContent = timeString;
+
+  setTimeout(updateTime, 1000); // Update the clock every second
+}
+
+updateTime();
