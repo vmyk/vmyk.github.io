13c5f2915a5d8c25b8bec8c3d605bea900879784
Seriplaca <calimiga70@gmail.com>
2023-07-17 17:22:27 +0100

    Added new files

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateTime() {
+    const clockElement = document.getElementById("clock");
+    const now = new Date();
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: false,
+      timeZone: "Europe/Lisbon"
+    };
+    const timeString = now.toLocaleTimeString("en-US", options);
+    clockElement.textContent = timeString;
+  
+    setTimeout(updateTime, 1000); // Update the clock every second
+  }
+  
+  updateTime();
+  
