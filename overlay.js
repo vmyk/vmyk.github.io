e2b81e862d919cc9abb322b8b7b4c307a0113ece
Miguel S <85355352+vmyk@users.noreply.github.com>
2023-12-13 00:17:59 +0000

    Delete overlay.js

--- overlay.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var contentWrapper = document.getElementById('content-wrapper');
-    var overlay = document.createElement('div');
-    overlay.className = 'overlay';
-    contentWrapper.appendChild(overlay);
-});
